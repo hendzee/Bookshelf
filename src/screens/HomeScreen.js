18620547e5ce916390e1e00338e79b187098390b
Virginia Hendras <hendzcode@gmail.com>
2020-02-25 22:09:40 +0700

    Try to structure css

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { SafeAreaView } from 'react-native';
-import { Layout, Text } from '@ui-kitten/components';
+import { Layout, Text, Input } from '@ui-kitten/components';
+import { generalSty } from '../styles';
 
-const HomeScreen = () => {
-    return (
-        <SafeAreaView>
-            <Text>Home</Text>
-        </SafeAreaView>        
-    );
+class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchValue: ''
+        }
+    }
+
+    render() {
+        return (
+            <SafeAreaView style={ generalSty.full }>
+                <Layout style={ generalSty.mainContainer }>
+                    
+                    {/** Search box */}
+                    <Layout style={{ marginBottom: 15 }}>
+                        <Input 
+                            placeholder='Search your book'
+                            value={ this.state.searchValue }
+                        />
+                    </Layout>
+
+                    {/** First slide  */}
+                    <Layout style={{ alignItems: 'baseline' }}>
+                        <Layout style={{ flexDirection: 'row', alignItems: 'baseline' }}>
+                            <Layout style={{ flex: 1 }}>
+                                <Text>On Your Hand</Text>
+                            </Layout>
+                            <Layout style={{ flex: 1, alignItems: 'flex-end' }}>
+                                <Text>See All</Text>
+                            </Layout>
+                        </Layout>
+                    </Layout>
+                </Layout>
+            </SafeAreaView>            
+        );
+    }
 }
 
-export default HomeScreen;
+export { HomeScreen };
