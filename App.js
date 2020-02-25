dd78eb9f8950d9d0985542b9b51e1254a03fbeb9
Virginia Hendras <hendzcode@gmail.com>
2020-02-25 21:01:29 +0700

    Clean code

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import {
-  ApplicationProvider,
-  Button,
-  Icon,
-  IconRegistry,
-  Layout,
-  Text,
-} from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import {
-  mapping,
-  light as theme,
-} from '@eva-design/eva';
+import { mapping, light as theme } from '@eva-design/eva';
 
 import AppNavigator from './src/screens/AppNavigator';
 
